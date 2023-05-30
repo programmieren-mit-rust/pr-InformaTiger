9213d3647f793b9fc4082c82215463e985e41d3f
Thomas Klaus <flyingsheep@tutanota.com>
2023-05-30 21:05:55 +0200

    kleiner Refactor: read_picture unter main() verschoben

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::fmt::{Display, Formatter};
 use std::fs::File;
 
+fn main() {
+    let pic_u8: PictureU8 = read_picture("src/gelbeOberleitung.png");
+    println!("PictureU8: {pic_u8}"); // :? führt hier dazu, dass data AUCH ausgegeben wird, das passt aber meist nicht in die Console
+
+    let pic_f32: PictureF32 = pic_u8.to_picture_f32();
+    println!("PictureF32: {pic_f32}");
+
+    let histograms = get_histogram(&pic_f32.to_picture_u8());
+    for i in 0..histograms.len() {
+        println!("Histogramm: {}", i + 1);
+        println!("{}", histograms[i]);
+    }
+}
+
 fn read_picture(path: &str) -> PictureU8 {
     //load picture
     let decoder = png::Decoder::new(File::open(path).unwrap());
@@ -18,20 +32,6 @@ fn read_picture(path: &str) -> PictureU8 {
         columns: info.width,
         color_channel_count: info.color_type.samples(),
         data: Vec::from(picture_data), //muss von &[u8] gecastet werden
-    }
-}
-
-fn main() {
-    let pic_u8: PictureU8 = read_picture("src/gelbeOberleitung.png");
-    println!("PictureU8: {pic_u8}"); // :? führt hier dazu, dass data AUCH ausgegeben wird, das passt aber meist nicht in die Console
-
-    let pic_f32: PictureF32 = pic_u8.to_picture_f32();
-    println!("PictureF32: {pic_f32}");
-
-    let histograms = get_histogram(&pic_f32.to_picture_u8());
-    for i in 0..histograms.len() {
-        println!("Histogramm: {}", i + 1);
-        println!("{}", histograms[i]);
     }
 }
 
