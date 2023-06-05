27d0055ad24ac05db36f43c9d71edf15573eb1e1
hoelzleva <129946095+devhoe@users.noreply.github.com>
2023-06-05 14:15:27 +0200

    some mod 'file' added

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let pic_u8: PictureU8 = read_picture("src/gelbeOberleitung.png");
     println!("PictureU8: {pic_u8}"); // :? führt hier dazu, dass data AUCH ausgegeben wird, das passt aber meist nicht in die Console
 
-    let pic_f32 = pic_u8.to_picture_u8();
+    let pic_f32 = pic_u8.to_picture_f32();
     println!("PictureF32: {pic_f32}");
 
     let histograms = get_histogram(&pic_f32.to_picture_u8());
