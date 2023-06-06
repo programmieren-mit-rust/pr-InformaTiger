d324fe8a22ba5bd15e135143a35fe55c4c90838b
hoelzleva <129946095+devhoe@users.noreply.github.com>
2023-06-06 09:04:20 +0200

    added comments

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use imsearch::{
 };
 
 fn main() {
-    let pic_u8: PictureU8 = read_picture_u8("Bilder Programmentwurf-20230603/bird.png");
-    println!("PictureU8: {pic_u8}"); // :? führt hier dazu, dass data AUCH ausgegeben wird, das passt aber meist nicht in die Console
 
-    let pic_f32: PictureF32 = pic_u8.to_picture_f32();
-    println!("PictureF32: {pic_f32}");
+
+
 
 }
