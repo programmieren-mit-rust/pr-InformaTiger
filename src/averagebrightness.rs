fe38ad86997762ebc633f05389529e54539a546b
ShroomAndShroomi <130099255+ShroomAndShroomi@users.noreply.github.com>
2023-06-13 18:29:40 +0200

    Aufgabe 2.a, Test geht jetzt

--- src/averagebrightness.rs
+++ src/averagebrightness.rs
@@ -4,7 +4,7 @@ use crate::picture::PictureF32;
 
 pub trait AverageBrightness {
     // Trait der Helligkeit
-    fn gray_intensity_single_val(&self,to_picture_f32: PictureF32)->f32;
+    fn gray_intensity_single_val(&self,red_colour_val: f32 , green_colour_val: f32, blue_colour_val: f32)->f32;
     fn gray_intensity_array(&self,to_picture_f32: PictureF32) -> Vec<f32>;
     fn average_brightness(&self, grayray: &Vec<f32>) -> f32;
 }
@@ -15,60 +15,31 @@ pub trait AverageBrightness {
 
 impl AverageBrightness for PictureF32 {
 
-    fn gray_intensity_single_val(&self,to_picture_f32: PictureF32)->f32{
-        let mut count_colour :usize = 0;
-        let mut r= 0f32;
-        let mut g= 0f32;
-        let mut b= 0f32;
+    fn gray_intensity_single_val(&self, red_colour_val: f32 , green_colour_val: f32, blue_colour_val: f32)->f32{
 
-        if to_picture_f32.color_channel_count == 3 {
-            r = to_picture_f32.data[count_colour ] ; //  wert1 -> Rot
-            g = to_picture_f32.data[count_colour + 1]; // wert2 -> Grün
-            b = to_picture_f32.data[count_colour + 2]; // wert3 -> Blau
-        }
-        else if to_picture_f32.color_channel_count == 4 {
-             r = to_picture_f32.data[count_colour]; // wert1 -> Rot
-             g = to_picture_f32.data[count_colour + 1 as usize]; //  wert2 -> Grün
-             b = to_picture_f32.data[count_colour + 2 as usize]; //  wert3 -> Blau
-        }
-        let singel_pixel_gray =((0.3 * r) + (0.59 * g) + (0.11 * b));
+        let singel_pixel_gray =((0.3 * red_colour_val) + (0.59 * green_colour_val) + (0.11 * blue_colour_val));
 
         return singel_pixel_gray;
-        }
+    }
 
     fn gray_intensity_array(&self, to_picture_f32: PictureF32) -> Vec<f32> {
         let mut grayray: Vec<f32> = Vec::new();
         let mut count_colour :usize = 0;
-        let mut pixel_count = 0u32;
 
 
+        if to_picture_f32.color_channel_count >= 3 {
+            while count_colour < to_picture_f32.data.len() {
 
+                let r = to_picture_f32.data[count_colour] ; //  wert1 -> Rot
+                let g = to_picture_f32.data[count_colour +1 ]; // wert2 -> Grün
+                let b = to_picture_f32.data[count_colour+2]; // wert3 -> Blau
 
-        if to_picture_f32.color_channel_count == 3 {
-            for _count_lines in 1..to_picture_f32.lines {
-                for _count_columns in 1..to_picture_f32.columns {
+                grayray.push(self.gray_intensity_single_val(r,g,b)) ;
+                count_colour += to_picture_f32.color_channel_count;
 
-                    let r = to_picture_f32.data[count_colour ] ; //  wert1 -> Rot
-                    let g = to_picture_f32.data[count_colour + 1]; // wert2 -> Grün
-                    let b = to_picture_f32.data[count_colour + 2]; // wert3 -> Blau
-                    count_colour = count_colour + 3;
-                    grayray.push((0.3 * r) + (0.59 * g) + (0.11 * b)) ;
-                    pixel_count = pixel_count + 1;
-                }
             }
-        } else if to_picture_f32.color_channel_count == 4 {
 
-            for _count_lines in 1..to_picture_f32.lines {
-                for _count_columns in 1..to_picture_f32.columns {
 
-                    let r = to_picture_f32.data[count_colour]; // wert1 -> Rot
-                    let g = to_picture_f32.data[count_colour + 1 as usize]; //  wert2 -> Grün
-                    let b = to_picture_f32.data[count_colour + 2 as usize]; //  wert3 -> Blau
-                    count_colour = count_colour + 4;
-                    grayray.push((0.3 * r) + (0.59 * g) + (0.11 * b)) ;
-                    pixel_count = pixel_count + 1;
-                }
-            }
         }
 
         self.average_brightness(&grayray);
@@ -78,15 +49,17 @@ impl AverageBrightness for PictureF32 {
 
 
     fn average_brightness(&self, grayray: &Vec<f32>) -> f32 {
-        let mut sum_grey = 0f32;
-        let pixels = grayray.len() as f32;
+        let mut sum_grey= 0f32;
+        let pixels = grayray.len() ;
+        let mut count= 0f32;
 
 
-        for count in grayray.iter() {
-            sum_grey = sum_grey + grayray[*count as usize];
+        while count < pixels as f32 {
+            sum_grey = sum_grey + grayray[count as usize];
+            count+=1.0;
         }
 
-        let averagebrightness = sum_grey / pixels;
+        let averagebrightness = sum_grey / pixels as f32;
         return averagebrightness;
     }
 
--- src/test_2a.rs
+++ src/test_2a.rs
@@ -1,61 +1,64 @@
-#[cfg(test)]
-use imsearch::{get_histogram, PictureU8, print_all_diagrams, read_picture};
-use imsearch::averagebrightness::AverageBrightness;
-use imsearch::picture::{Picture, PictureF32};
-use crate::PictureF32;
-use crate::AverageBrightness;
+use crate::averagebrightness::AverageBrightness;
+use crate::picture::PictureF32;
 
 
-mod test_2a {
-    use crate::picture::PictureF32;
-    use super::*;
 
-    struct MockPicture {
-        lines: u32,
-        columns: u32,
-        color_channel_count: usize,
-        data: Vec<f32>,
-    }
 
-    #[test]
-    fn test_2_a() {
-        let mock_picture = MockPicture {
-            lines: 10,
-            columns: 10,
-            color_channel_count: 3,
-            data: Vec::new(),
-        };
+#[test]
+fn test_gray_intensity_single_val() {
+    let picture = PictureF32 {
+        lines: 0,
+        data: vec![0.1, 0.2, 0.12], // Rot: 255, Grün: 128, Blau: 64
+        color_channel_count: 3,
+        columns: 0,
+    };
 
-        let picture_f32 = PictureF32 {
-            lines: 10,
-            columns: 10,
-            color_channel_count: 3,
-            data: vec![0.20, 0.23, 0.34, 0.12, 0.25, 0.15, 0.176, 0.145, 0.298], // Sample data for r, g, b values
-        };
+    let result = picture.gray_intensity_single_val(0.1, 0.2, 0.12);
 
-        let grayray = mock_picture.gray_intensity_array(picture_f32.clone());
+    assert_eq!(result, 0.1612); // Erwartetes Ergebnis basierend auf der Berechnung
 
-        let expected_grayray = vec![0.2331, 0.2, 0.17113];
-
-        // Assert that the calculated grayray matches the expected grayray
-        assert_eq!(grayray, expected_grayray);
-
-        let average_brightness = mock_picture.averagebrightness(&grayray);
-
-        let expected_average_brightness = 0.20174333;
-
-        // Assert that the calculated average brightness matches the expected average brightness
-        assert_eq!(average_brightness, expected_average_brightness);
-
-        let r = picture_f32.data[0];
-        let g = picture_f32.data[1];
-        let b = picture_f32.data[2];
-
-        let calculated_gray = mock_picture.gray_intensity_single_val(picture_f32.clone());
-
-        let expected_gray = (0.3 * r) + (0.59 * g) + (0.11 * b);
-
-        // Assert that the calculated gray value matches the expected gray value
-        assert_eq!(calculated_gray, expected_gray);
-    }
+    // Weitere Testfälle hinzufügen...
 }
+
+#[test]
+fn test_gray_intensity_array() {
+    let picture = PictureF32 {
+        lines: 0,
+        data: vec![0.1, 0.2, 0.25, 0.12, 0.22, 0.1], // Zwei Pixel: Rot: 255, Grün: 128, Blau: 64 und Rot: 200, Grün: 100, Blau: 50
+        color_channel_count: 3,
+        columns: 0,
+    };
+
+    let result = picture.gray_intensity_array(picture.clone());
+
+    assert_eq!(result, vec![0.1755,0.17679998]); // Erwartetes Ergebnis basierend auf der Berechnung
+
+    // Weitere Testfälle hinzufügen...
+}
+
+#[test]
+fn test_average_brightness() {
+    let picture = PictureF32 {
+        lines: 0,
+        data: vec![0.23, 0.188, 0.256, 0.2, 0.1, 0.1], // Zwei Pixel: Rot: 255, Grün: 128, Blau: 64 und Rot: 200, Grün: 100, Blau: 50
+        color_channel_count: 3,
+        columns: 0,
+    };
+
+    let grayray = vec![0.20808, 0.13]; // Beispielwerte für den Test
+
+    let result = picture.average_brightness(&grayray);
+
+    assert_eq!(result, 0.16904); // Erwartetes Ergebnis basierend auf der Berechnung
+
+    // Weitere Testfälle hinzufügen...
+}
+
+
+
+
+
+
+
+
+
