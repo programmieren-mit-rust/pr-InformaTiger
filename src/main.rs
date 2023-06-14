79a768954d057e3507cf8bf30d4c1c39e53c4e86
Thomas Klaus <flyingsheep@tutanota.com>
2023-06-14 18:06:23 +0200

    fixed stuff in main to remove project errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use imsearch::picture::{AverageBrightness, Picture, PictureF32};
+use imsearch::picture::{AverageBrightness, Picture};
 use imsearch::{get_datastore_path, suchindex};
 use imsearch::{get_histogram, print_all_diagrams, read_picture, PictureU8};
 
@@ -12,7 +12,7 @@ fn main() {
     print_all_diagrams(histograms, pic_u8.color_channel_count); //TODO Werte nach Balken schreiben? (auf gleicher höhe (nach 40 Zeichen) oder direkt hinter Balken?) -> als optionales Feature?
 
     //Aufruf +Ausgabe Averagebrightness
-    let grayray = gray_intensity_array(pic_u8.to_picture_f32());
-    let average_brightness = pic_u8.average_brightness(&grayray); // Aufruf von averagebrightness
+    let grayray = pic_u8.to_picture_f32().gray_intensity_array();
+    let average_brightness = pic_u8.to_picture_f32().average_brightness(&grayray); // Aufruf von averagebrightness
     println!("Averagebrightness: {average_brightness}");
 }
