21ec5d79cca01c5770acc64f5bcc07d8dee35749
ShroomAndShroomi <130099255+ShroomAndShroomi@users.noreply.github.com>
2023-06-15 17:32:33 +0200

    User_Input : hier ne kleien Abfrage für den User (aber so wie die Implementiert wurde passt ,die nicht mehr zum Code). Ideen?

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ const PICTURE_FILEPATH: &str = "src/tests/files/pictures_for_testing/bird.png";
 fn main() {
 
     // Eingabe lesen
+    println!("Bitte geben Sie den Datei-Pfad Ihres Bildes ein:");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Fehler beim Lesen der Eingabe");
     // Das eingegebene Wort extrahieren
     let file_path = input.trim();
 
-    PICTURE_FILEPATH= file_path ;
-    let pic_u8: PictureU8 = read_picture(PICTURE_FILEPATH);
+    let pic_u8: PictureU8 = read_picture(file_path);
     println!("PictureU8: {pic_u8}"); // :? führt hier dazu, dass data AUCH ausgegeben wird, das passt aber meist nicht in die Console
     // User Input (Eventuell noch mit GUI)
-    println!("Bitte geben Sie den Datei-Pfad Ihres Bildes ein:");
+
 
     let histograms = get_histogram(&pic_u8);
     print_all_diagrams(histograms, pic_u8.color_channel_count); //TODO Werte nach Balken schreiben? (auf gleicher höhe (nach 40 Zeichen) oder direkt hinter Balken?) -> als optionales Feature?
