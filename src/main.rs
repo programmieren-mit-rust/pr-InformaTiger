f135bad777f39e52228a2eb4cec0dea307059a43
ShroomAndShroomi <130099255+ShroomAndShroomi@users.noreply.github.com>
2023-06-16 08:52:35 +0200

    User_Input : hier ne kleien Abfrage für den User (aber so wie die Implementiert wurde passt ,die nicht mehr zum Code). Ideen?

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ use std::io;
 const PICTURE_FILEPATH: &str = "src/tests/files/pictures_for_testing/bird.png";
 
 fn main() {
+    //Eingabe vom User , der Bilder die in den Search Index kommen / Suchpool
 
-    // Eingabe lesen
+
+
+
+    // Eingabe lesen Such Bild
     println!("Bitte geben Sie den Datei-Pfad Ihres Bildes ein:");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Fehler beim Lesen der Eingabe");
@@ -25,4 +29,38 @@ fn main() {
     let grayray = pic_u8.to_picture_f32().gray_intensity_array();
     let average_brightness = pic_u8.to_picture_f32().average_brightness(&grayray); // Aufruf von averagebrightness
     println!("Averagebrightness: {average_brightness}");
+}
+
+pub fn einlesen()->String {
+    println!("Bitte geben Sie den Datei-Pfad ihres Bilder-Ordners oder Bildes an ,in dem sie Bilder suchen wollen:(Schreiben Sie 'nein', wenn sie dies Übersprinen wollen)");
+    let mut input_searchlib = String::new();
+    io::stdin().read_line(&mut input_searchlib).expect("Fehler beim Lesen der Eingabe");
+    // Das eingegebene Wort extrahieren
+
+    let no= "nein";
+    match input_lib      {
+        no =>{ println!("Übersprungen!");}
+
+        _ => {let input_lip = input_searchlib.trim();
+            ;
+        }
+    return input_lib;
+    }
+
+}
+
+pub fn wiederhol_eingbe(){
+    print!("Wollen Sie ihre Suchbibliothek noch erweitern?(ja/nein):");
+    let mut antwort = String::new();
+    io::stdin().read_line(&mut antwort).expect("Fehler beim Lesen der Eingabe");
+    let final_answer = input_searchlib.trim();
+
+    match final_answer{
+        f if f.contains("nein")=>{println!("OK, Eingabe wird übersprungen");}
+        f if f.contains("ja")=>{einlesen();}
+        _ => {wiederhol_eingbe();}
+
+
+    }
+
 }
