fd9b82097838a2b4cdb37087afc1e6c2d34d235e
ShroomAndShroomi <130099255+ShroomAndShroomi@users.noreply.github.com>
2023-06-16 17:19:18 +0200

    Compare_picture added rust docu

--- src/compare_pictures.rs
+++ src/compare_pictures.rs
@@ -3,11 +3,31 @@ use crate::suchindex::read_data_from_datastore;
 use crate::suchindex::SearchIndex;
 use std::error::Error;
 
+/// A trait for comparing pictures.
 pub trait ComparePicture {
+    /// Calculates the differences in brightness between the search index and the provided index.
+    ///
+    /// # Arguments
+    ///
+    /// * `search_index` - The search index to compare with.
+    ///
+    /// # Returns
+    ///
+    /// A vector containing the differences in brightness between the pictures.
     fn difference_brightnesses(&self, search_index: &SearchIndex) -> Vec<f32>;
 }
 
 impl ComparePicture for SearchIndex {
+    /// Implementation of the `ComparePicture` trait for the `SearchIndex` struct.
+    /// Calculates the differences in brightness between the search index and the provided index.
+    ///
+    /// # Arguments
+    ///
+    /// * `search_index` - The search index to compare with.
+    ///
+    /// # Returns
+    ///
+    /// A vector containing the differences in brightness between the pictures.
     fn difference_brightnesses(&self, search_index: &SearchIndex) -> Vec<f32> {
         let data: Vec<SearchIndex> =
             read_data_from_datastore().expect("Fehler beim Lesen der Daten aus dem Datastore");
