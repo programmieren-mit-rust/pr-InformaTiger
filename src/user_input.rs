d466782f36f8e198e5993cb656600bda3fa6f621
ShroomAndShroomi <130099255+ShroomAndShroomi@users.noreply.github.com>
2023-06-16 15:07:06 +0200

    User_Input : Added rust docu to file user_input.rs

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,6 +1,19 @@
+/// This module provides functions for user input and picture analysis.
+///
 use std::io;
 use crate::suchindex::analyse_pictures;
 
+/// Prompts the user to enter the file path of a directory containing images or a single image file from which to search for images.
+/// Performs an analysis of the entered images.
+/// Returns the entered file path.
+///
+/// # Examples
+///
+/// ```
+/// use imsearch::user_input::eingabe;
+/// let file_path = eingabe();
+/// println!("Entered file path: {}", file_path);
+/// ```
 pub fn eingabe() -> String {
     println!("Bitte geben Sie den Datei-Pfad ihres Bilder-Ordners oder Bildes an ,aus dem sie Bilder suchen wollen:");
     let mut input_searchlib = String::new();
@@ -17,6 +30,15 @@ pub fn eingabe() -> String {
 
 }
 
+/// Prompts the user to extend their search library or not.
+/// If "ja" (yes) is selected, calls the `eingabe` function and repeats the input.
+///
+/// # Examples
+///
+/// ```
+/// use imsearch::user_input::wiederhol_eingabe;
+/// wiederhol_eingabe();
+/// ```
 pub fn wiederhol_eingabe() {
     println!("Wollen Sie ihre Suchbibliothek noch erweitern?(ja/nein):");
     let mut antwort = String::new();
@@ -38,6 +60,17 @@ pub fn wiederhol_eingabe() {
         }
     }
 }
+
+/// Prompts the user to enter the file path of an image for which they want to search for similar images.
+/// Performs an analysis of the entered image.
+/// Returns the entered file path as a String.
+///
+/// # Examples
+///
+/// ```
+/// use imsearch::user_input::eingabe_suchbild;
+/// let final_picture = eingabe_suchbild();
+/// ```
 
 pub fn eingabe_suchbild() -> String {
     println!("Suche ähnliche Bilder für (Eingabe Datei-Pfad für Bild):");
