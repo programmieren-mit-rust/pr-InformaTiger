413a3d369a20404593a642699d42454f65b3435e
hoelzleva <129946095+devhoe@users.noreply.github.com>
2023-06-18 11:30:22 +0200

    edited main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use imsearch::{get_histogram, print_all_diagrams, read_picture, PictureU8, get_average_brightness_of_picture, get_top_five_similar_pictures, print_calculated_similar_pictures};
+use imsearch::{get_histogram, print_all_diagrams, read_picture, PictureU8, get_average_brightness_of_picture, get_top_five_similar_pictures, print_calculated_similar_pictures, get_pictures_from_user};
 use imsearch::search_index::{analyse_pictures, generate_suchindex_to_file};
+use imsearch::user_input::input_search_image;
 
 const PICTURE_FILEPATH_BIRD: &str = "src/tests/files/pictures_for_testing/bird.png";
 const PICTURE_FILEPATH_FLOWER2: &str =
@@ -7,17 +8,14 @@ const PICTURE_FILEPATH_FLOWER2: &str =
 
 fn main() {
 
-    // get_pictures_from_user();
-    //
-    // //Input User: SearchImage
-    // let picture_path = input_search_image();
+    get_pictures_from_user();
 
-    analyse_pictures("src/tests/files/pictures_for_testing/").expect("TODO: panic message");
+    //Input User: SearchImage
+    let picture_path = input_search_image();
 
-    let similar_five_pictures = get_top_five_similar_pictures(PICTURE_FILEPATH_BIRD).unwrap();
+
+    let similar_five_pictures = get_top_five_similar_pictures(picture_path.as_str()).unwrap();
     print_calculated_similar_pictures(similar_five_pictures);
-
-
 
     println!("Information on the picture you provided:");
     //Aufruf +Ausgabe Averagebrightness
@@ -30,7 +28,6 @@ fn main() {
     println!("PictureU8: {pic_u8}");
     let histograms = get_histogram(&pic_u8);
     print_all_diagrams(histograms);
-
 
 }
 
